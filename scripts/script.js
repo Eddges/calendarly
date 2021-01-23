d07fe43e899fad0137e5e6d11613b643ac0428de
Eddges <shivamshekhar.i7@gmail.com>
2021-01-23 23:15:26 +0530

    Current month and previous months overflowing dates rendered

--- scripts/script.js
+++ scripts/script.js
@@ -15,13 +15,30 @@ var dayArray = [
 var renderCalendar = function () {
     var monthNameElement = document.querySelector('.MonthName');
     var currentDateElement = document.querySelector('.CurrentDate');
-    var today = new Date();
-    console.log('Month : ', today.getMonth());
+    var datesElement = document.querySelector('.Dates');
+    var newDate = new Date();
+    var currentDateNumber = newDate.getDate();
+    var currentDayNumber = newDate.getDay();
+    var currentMonthNumber = newDate.getMonth();
+    var currentYearNumber = newDate.getFullYear();
+    var firstDayOfThisMonth = new Date(2021, newDate.getMonth(), 1);
+    var lastDayOfThisMonth = new Date(2021, newDate.getMonth() + 1, 0);
+    var currentDayOfTheWeek = firstDayOfThisMonth.getDay();
+    var lastDateOfPrevMonth = new Date(2021, newDate.getMonth(), 0).getDate();
+    console.log('last day of Month : ', lastDayOfThisMonth.getDate());
     if (monthNameElement) {
-        monthNameElement.innerHTML = dayArray[today.getMonth()];
+        monthNameElement.innerHTML = dayArray[currentMonthNumber];
     }
     if (currentDateElement) {
-        currentDateElement.innerHTML = today.toDateString();
+        currentDateElement.innerHTML = newDate.toDateString();
+    }
+    if (datesElement) {
+        for (var i = lastDateOfPrevMonth - currentDayOfTheWeek + 1; i <= lastDateOfPrevMonth; i++) {
+            datesElement.innerHTML += "<span class='Date PrevDate'>" + i + "</span>";
+        }
+        for (var i = 1; i <= lastDayOfThisMonth.getDate(); i++) {
+            datesElement.innerHTML += "<span class='Date " + (i === currentDateNumber ? 'Active' : '') + "'>" + i + "</span>";
+        }
     }
 };
 renderCalendar(0);
--- scripts/script.ts
+++ scripts/script.ts
@@ -17,15 +17,37 @@ const renderCalendar: (contextMonth : number) => void = () => {
 
     const monthNameElement = document.querySelector('.MonthName')
     const currentDateElement = document.querySelector('.CurrentDate')
+    const datesElement = document.querySelector('.Dates')
 
-    const today: Date = new Date();
-    console.log('Month : ', today.getMonth())
+    const newDate: Date = new Date();
+
+    const currentDateNumber : number = newDate.getDate()
+    const currentDayNumber : number = newDate.getDay()
+    const currentMonthNumber : number = newDate.getMonth()
+    const currentYearNumber : number = newDate.getFullYear()
+
+    const firstDayOfThisMonth : Date = new Date(2021, newDate.getMonth(), 1)
+    const lastDayOfThisMonth : Date = new Date(2021, newDate.getMonth() + 1, 0)
+
+    const currentDayOfTheWeek : number = firstDayOfThisMonth.getDay()
+    const lastDateOfPrevMonth : number = new Date(2021, newDate.getMonth(), 0).getDate()
+
+    console.log('last day of Month : ', lastDayOfThisMonth.getDate())
 
     if(monthNameElement) {
-        monthNameElement.innerHTML = dayArray[today.getMonth()]
+        monthNameElement.innerHTML = dayArray[currentMonthNumber]
     }
     if(currentDateElement) {
-        currentDateElement.innerHTML = today.toDateString()
+        currentDateElement.innerHTML = newDate.toDateString()
+    }
+
+    if(datesElement) {
+        for(let i = lastDateOfPrevMonth - currentDayOfTheWeek + 1; i<=lastDateOfPrevMonth; i++){
+            datesElement.innerHTML += `<span class='Date PrevDate'>${i}</span>`
+        }
+        for(let i = 1; i<=lastDayOfThisMonth.getDate(); i++) {
+            datesElement.innerHTML += `<span class='Date ${i === currentDateNumber ? 'Active' : ''}'>${i}</span>`
+        }
     }
     
 };
