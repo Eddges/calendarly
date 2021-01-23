58d1f95d8d12341273d2f8d4e7dd72ddfd9ff356
Eddges <shivamshekhar.i7@gmail.com>
2021-01-23 19:44:23 +0530

    Current month and Date display done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,27 @@
-console.log('Hey peter');
+var dayArray = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+var renderCalendar = function () {
+    var monthNameElement = document.querySelector('.MonthName');
+    var currentDateElement = document.querySelector('.CurrentDate');
+    var today = new Date();
+    console.log('Month : ', today.getMonth());
+    if (monthNameElement) {
+        monthNameElement.innerHTML = dayArray[today.getMonth()];
+    }
+    if (currentDateElement) {
+        currentDateElement.innerHTML = today.toDateString();
+    }
+};
+renderCalendar(0);
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,1 +1,33 @@
-console.log('Hey peter')
+const dayArray : string[] = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
+const renderCalendar: (contextMonth : number) => void = () => {
+
+    const monthNameElement = document.querySelector('.MonthName')
+    const currentDateElement = document.querySelector('.CurrentDate')
+
+    const today: Date = new Date();
+    console.log('Month : ', today.getMonth())
+
+    if(monthNameElement) {
+        monthNameElement.innerHTML = dayArray[today.getMonth()]
+    }
+    if(currentDateElement) {
+        currentDateElement.innerHTML = today.toDateString()
+    }
+    
+};
+
+renderCalendar(0);
